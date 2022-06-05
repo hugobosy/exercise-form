b025a242b74934c8e0f00c9d02bbd622bf8fda66
HugoBosy <hkliszcz@gmail.com>
2022-06-05 23:19:09 +0200

    add styles

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,15 +5,19 @@ export const Login = props => {
     return (
         <form onSubmit={submit}>
             <h1>Logowanie</h1>
-            <label>
-                Email:
-                <input type="text" name="email" value={user.email} onChange={props.change}/>
-            </label>
-            <label>
-                Password:
-                <input type={user.showPass ? 'text' : 'password'} name="password" value={user.password}
-                       onChange={props.change}/>
-            </label>
+            <p>
+                <label>
+                    Email:
+                    <input type="text" name="email" value={user.email} onChange={props.change}/>
+                </label>
+            </p>
+           <p>
+               <label>
+                   Password:
+                   <input type={user.showPass ? 'text' : 'password'} name="password" value={user.password}
+                          onChange={props.change}/>
+               </label>
+           </p>
             <button type="submit">Login</button>
             <button type="button" onClick={showPass}>{user.showPass ? "Hidden password" : "Show password"}</button>
         </form>
