dadf086e658f467967f1564f31c74efd574d2910
HugoBosy <hkliszcz@gmail.com>
2022-06-05 12:28:24 +0200

    add form structure, state & Box component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [user, setUser] = useState( {
+        email: '',
+        password: '',
+    })
+
+    return (
+        <div className="App">
+            <form>
+                <label>
+                    Email:
+                    <input type="text" value={user.email} onChange={e => setUser(user =>({
+                        ...user,
+                        email: e.target.value,
+                    }))}/>
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={user.password} onChange={e => setUser(user => ({
+                        ...user,
+                        password: e.target.value,
+                    }))}/>
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
